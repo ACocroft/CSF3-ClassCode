dae22c5c039b1896029839f5a7b8a092fdaccbcf
Andrew Cocroft <drew.cocroft@outlook.com>
2023-05-09 11:47:53 -0500

    Finally finished Block3Labs

--- Block3Labs/10DigitFib.cs
+++ Block3Labs/10DigitFib.cs
@@ -8,5 +8,20 @@ namespace Block3Labs
 {
     internal class _10DigitFib
     {
+        static void Main(string[] args)
+        {
+            long n1 = 1, n2 = 0, n3 = 0;
+            Stack<long> fibNumber = new();
+
+            while (n3 <= 1000000000)
+            {
+                Console.WriteLine(n3);
+                fibNumber.Push(n3);
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine(fibNumber.Count()+1);
+        }
     }
 }
